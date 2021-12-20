fa88d6eb21ebe02fc17c2887f843718ed1b37130
Placido Fernandez Declara <placido.fernandez@cern.com>
2021-12-21 00:14:08 +0100

    first working version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module apunta
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+	// "net/url"
+)
+
+type Entry struct {
+    Who      string
+    Currency string
+    Quantity string
+}
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+func indexHandler(entries []Entry) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	  if r.Method != http.MethodPost {
+	    tpl.Execute(w, entries)
+	    return
+	  }
+
+		entry := Entry{
+			Who: r.FormValue("wwhhoo"),
+			Currency: r.FormValue("acurrency"),
+			Quantity: r.FormValue("aquantity"),
+		}
+
+		entries = append(entries, entry)
+
+	  for _, myentry := range entries {
+			fmt.Println("Who is: ", myentry.Who)
+			fmt.Println("Currency is: ", myentry.Currency)
+			fmt.Println("Quantity is: ", myentry.Quantity)
+	  }
+
+		tpl.Execute(w, struct{ Success bool }{true})
+	}
+}
+
+
+func genEntry() Entry {
+	entry := Entry{
+		Who: "who",
+		Currency: "currency",
+		Quantity: "quantity",
+	}
+	return entry;
+}
+
+func main() {
+	port := "3000"
+
+	fs := http.FileServer(http.Dir("assets"))
+
+	// Entries stuff
+	myentries := make([]Entry, 0)
+	myentries = append(myentries, genEntry());
+	myentries = append(myentries, genEntry());
+	myentries = append(myentries, genEntry());
+
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/", indexHandler(myentries))
+	http.ListenAndServe(":"+port, mux)
+}
